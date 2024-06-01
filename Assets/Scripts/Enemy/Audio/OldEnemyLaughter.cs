eea628ea563c265d526458ac9d441912ee50708e
Absurdius <e94doclrd@hotmail.com>
2024-06-01 11:04:54 +0200

    Fix clown laughter for circus tent scene

---
+++ Assets/Scripts/Enemy/Audio/OldEnemyLaughter.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OldEnemyLaughter : MonoBehaviour
+{
+    public EnemyController enemyController;
+    public AudioClip[] laughs;
+
+    private AudioSource audioSource;
+    private EnemyController.EnemyState currentState;
+
+
+
+    void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        currentState = enemyController.GetEnemyState();
+    }
+
+    void Update()
+    {
+        if (UIStateManager.currentState == UIStateManager.UIState.PLAYING)
+        {
+            // Check if the enemy state has changed
+            if (enemyController.GetEnemyState() != currentState)
+            {
+                currentState = enemyController.GetEnemyState();
+                if (!audioSource.isPlaying)
+                {
+                    audioSource.PlayOneShot(laughs[UnityEngine.Random.Range(0, laughs.Length)], 1.0f);
+                }
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+}
