c63dfaee2f48ef500702cab3041a0bcb337b909e
Absurdius <e94doclrd@hotmail.com>
2024-06-01 19:23:43 +0200

    Add arcade mode to main menu

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -5,23 +5,55 @@ public class MainMenuManager : MonoBehaviour
 {
 
     [SerializeField] private GameObject tutorialUI; 
+    [SerializeField] private GameObject gameSelectUI; 
 
     public void StartGame()
     {
-        //SceneManager.LoadScene("SliceScene");
-        SceneManager.LoadScene(1);
-        //SceneManager.LoadScene("DevScene");
+        ToggleGameSelect();
+    }
+
+    public void playSurvival()
+    {
+        SceneManager.LoadScene("MainGame");
+    }    
+    
+    public void playArcade()
+    {
+        SceneManager.LoadScene("CircusTentScene");
     }
 
     public void ToggleTutorial()
     {
         if (tutorialUI == null) { Debug.LogError("[MainMenuManger] GameObject tutorialUI not found"); }
+        if (gameSelectUI.activeSelf)
+        {
+            gameSelectUI.SetActive(false);
+        }
 
         if (tutorialUI.activeSelf) { 
             tutorialUI.SetActive(false); 
         } 
         else { 
             tutorialUI.SetActive(true);
+        }
+    }
+
+    public void ToggleGameSelect()
+    {
+        if (gameSelectUI == null) { Debug.LogError("[MainMenuManger] GameObject gameSelectUI not found"); }
+
+        if (tutorialUI.activeSelf)
+        {
+            tutorialUI.SetActive(false);
+        }
+
+        if (gameSelectUI.activeSelf)
+        {
+            gameSelectUI.SetActive(false);
+        }
+        else
+        {
+            gameSelectUI.SetActive(true);
         }
     }
 
